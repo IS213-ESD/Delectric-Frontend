447b261a5d432b734ea146a03b615fb0123dd710
Shawnkharece <shawnkharece@gmail.com>
2024-03-13 15:48:32 +0800

    EV-01 removed

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
-import { useMainStore } from '@/stores/main.js'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
+import App from './App.vue';
+import router from './router';
+import { useMainStore } from '@/stores/main.js';
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
-import './css/main.css'
+import './css/main.css';
 
 // Init Pinia
-const pinia = createPinia()
+const pinia = createPinia();
 
 // Create Vue app
 createApp(App)
@@ -17,38 +17,39 @@ createApp(App)
   .use(pinia)
   .use(VueGoogleMaps, {
     load: {
-      key: 'AIzaSyA-R-V5D-Di4Y4NcmW-DFt0Aez9AOxj5X0', // Add your Google Maps API key here
-      libraries: 'places' // Add any additional libraries if needed
-    }
+      key: '', // Add your Google Maps API key here
+      libraries: 'places', // Add any additional libraries if needed
+    },
   })
-  .mount('#app')
+  .mount('#app');
 
 // Init main store
-const mainStore = useMainStore(pinia)
+const mainStore = useMainStore(pinia);
 
 // Fetch sample data
-mainStore.fetchSampleClients()
-mainStore.fetchSampleHistory()
+mainStore.fetchSampleClients();
+mainStore.fetchSampleHistory();
 
 // Dark mode
 // Uncomment, if you'd like to restore persisted darkMode setting, or use `prefers-color-scheme: dark`. Make sure to uncomment localStorage block in src/stores/darkMode.js
-// import { useDarkModeStore } from './stores/darkMode'
+import { useDarkModeStore } from './stores/darkMode';
 
-// const darkModeStore = useDarkModeStore(pinia)
+const darkModeStore = useDarkModeStore(pinia);
 
-// if (
-//   (!localStorage['darkMode'] && window.matchMedia('(prefers-color-scheme: dark)').matches) ||
-//   localStorage['darkMode'] === '1'
-// ) {
-//   darkModeStore.set(true)
-// }
+if (
+  (!localStorage['darkMode'] &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+  localStorage['darkMode'] === '1'
+) {
+  darkModeStore.set(true);
+}
 
 // Default title tag
-const defaultDocumentTitle = 'Admin One Vue 3 Tailwind'
+const defaultDocumentTitle = 'Delectric';
 
 // Set document title from route meta
 router.afterEach((to) => {
   document.title = to.meta?.title
     ? `${to.meta.title} — ${defaultDocumentTitle}`
-    : defaultDocumentTitle
-})
+    : defaultDocumentTitle;
+});
