cba7c31bd87822864d89b8cf1bc1c11236683a18
Derrick-lkh <derrick872k@gmail.com>
2024-03-20 22:12:09 +0800

    current/ upcoming booking w status

---
+++ src/views/Bookings.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
+</script>
+
+<template>
+  <LayoutAuthenticated>
+    <div class="grid grid-cols-1 gap-5 px-5 mt-5">
+        <div class="card bg-base-100 flex justify-between">
+            <figure><img src="https://static1.straitstimes.com.sg/s3fs-public/styles/large30x20/public/articles/2021/01/09/us-automobile-environment-tesla-052911.jpg?VersionId=VBWE6tjAISYWZ3s1BLeqAM7N_IKrMyOW"/></figure>
+            <div class="card-body">
+                <div class="flex justify-between items-baseline">
+                    <span class="text-slate-400 text-xs">Est. 3 Hours to fully charged</span>
+                    <span>⚡60%</span>
+                </div>
+                <progress class="progress h-3 progress-accent w-100" value="60" max="100"></progress>
+                <div class="flex justify-between align-middle">
+                    <h2 class="card-title">NAFA Campus Charging Station</h2>
+                </div>
+                <span class="text-slate-400">190 Middle Rd, Singapore 188979</span>
+                <span class="text-slate-400">Booked From: 1PM-3PM (30 Mins remaining)</span>
+                <div class="card-actions mt-5">
+                    <button class="btn btn-primary btn-outline w-full">END CHARGING</button>
+                </div>
+            </div>
+        </div>
+        <div class="card bg-base-100">
+            <figure><img src="https://static1.straitstimes.com.sg/s3fs-public/styles/large30x20/public/articles/2021/01/09/us-automobile-environment-tesla-052911.jpg?VersionId=VBWE6tjAISYWZ3s1BLeqAM7N_IKrMyOW"/></figure>
+            <div class="card-body">
+                <progress class="progress h-3 progress-neutral w-100" value="30" max="100"></progress>
+                <div class="flex justify-between align-middle">
+                    <h2 class="card-title">NAFA Campus Charging Station</h2>
+                </div>
+                <span class="text-slate-400">190 Middle Rd, Singapore 188979</span>
+                <span class="text-slate-400">Booked From: 10PM-12AM (5 Hours to booking)</span>
+                <div class="card-actions mt-5">
+                    <button class="btn btn-primary btn-outline w-full">CANCEL BOOKING</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  </LayoutAuthenticated>
+</template>
